f0470be1389ee8694fb574c2ab00c232de73b85f
george <gkephart@cnm.edu>
2019-06-24 16:30:54 -0600

    committing and pushing and calling it a day

--- src/pages/userPosts/PostCard.js
+++ src/pages/userPosts/PostCard.js
@@ -1,18 +1,13 @@
 import React from "react";
 
-export const  PostCard = ({post}) => {
-
-	console.log(post);
-
-	return (
-		<div className="card text-white bg-dark mb-3">
-			<div className="card-body">
-				<h5 className="card-title">{post.title}</h5>
-				<p className="card-text">{post.body}</p>
-				<p className="card-text">
-					<small className="text-muted">{post.username}</small>
-				</p>
-			</div>
+export const PostCard = ({post}) => (
+	<div className="card text-white bg-dark mb-3">
+		<div className="card-body">
+			<h5 className="card-title">{post.title}</h5>
+			<p className="card-text">{post.body}</p>
+			<p className="card-text">
+				<small className="text-muted">{post.username}</small>
+			</p>
 		</div>
-	)
-};
+	</div>
+);
--- src/shared/misc/http-config.js
+++ src/shared/misc/http-config.js
@@ -1,17 +1,35 @@
 import axios from "axios/index";
 
-export const httpConfig = axios.create();
 
-httpConfig.interceptors.response.use(function ({data, headers} ) {
-	if(data.status === 200) {
-		return data.data !== null
-			? {message: null, data: data.data, status: 200, type: " alert alert-success", headers: {...headers}}
-			: {message: data.message, status: 200, type: " alert alert-success", data: null, headers: {...headers}};
+export async function useAxios(path, verb, requestObject = null) {
+
+	const http = axios.create();
+
+	http.interceptors.response.use(function({data, headers}) {
+		if(data.status === 200) {
+			return data.data !== null
+				? {message: null, data: data.data, status: 200, type: " alert alert-success", headers: {...headers}}
+				: {message: data.message, status: 200, type: " alert alert-success", data: null, headers: {...headers}};
+		}
+		return {message: data.message, status: data.status, type: "alert alert-danger", data: null, headers: {...headers}}
+
+	}, function(error) {
+		// Do something with response error
+		console.log(error);
+		return Promise.reject(error);
+	});
+
+
+	switch(verb) {
+
+		case "get":
+			return await http.get(path);
+
+		case "post":
+			return await requestObject ? http.post(path, requestObject) : "no requestObject";
+
+			default:
+			return "request was not properly formatted"
+
 	}
-	return {message: data.message, status: data.status, type: "alert alert-danger", data: null, headers: {...headers}}
-
-}, function (error) {
-	// Do something with response error
-	console.log(error);
-	return Promise.reject(error);
-});
+}
